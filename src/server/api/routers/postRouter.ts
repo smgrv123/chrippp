f6e954fc0c1a6cb3e81a6d3bf40c969b641b036b
Sumrit Grover <sumritgrover1@gmail.com>
2023-03-29 11:53:23 +0530

    additions and changes - post wizard - post view - next image included - cleanup and logic checking - post router updation

--- src/server/api/routers/postRouter.ts
+++ src/server/api/routers/postRouter.ts
@@ -1,9 +1,48 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { clerkClient } from "@clerk/nextjs/server";
+import type { User } from "@clerk/nextjs/dist/api";
+import { TRPCError } from "@trpc/server";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
 
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((item) => item.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author || !author.username) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author not found for post",
+        });
+      }
+
+      return {
+        post,
+        author:{
+            ...author,
+            username: author.username
+        },
+      };
+    });
   }),
 });
